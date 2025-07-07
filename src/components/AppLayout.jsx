87bc3c3761015d39d12774424404c44ca55c62b1
hyahn0 <gmldbs0725@gmail.com>
2025-07-07 11:51:57 +0900

    feat/2 내 정보 및 요약 페이지, 상단 바 구현

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Sidebar from "./SideBar";
+// import Sidebar from "./SideBar";
 import Topbar from "./TopBar";
 import "./AppLayout.css";
 
 const AppLayout = ({ children }) => {
   return (
     <div className="layout">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="main-content">
         <Topbar />
         <div className="page-content">{children}</div>
